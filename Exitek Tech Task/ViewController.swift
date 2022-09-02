fd9f60f796c4a905afb95aeeb4734bff0ed7b58b
Behichev <ivanbehichev@gmail.com>
2022-09-02 10:58:54 +0300

    Initial Commit

---
+++ Exitek Tech Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exitek Tech Task
+//
+//  Created by Иван Бегичев on 02.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
